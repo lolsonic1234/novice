e8eb3568a3a6a16a5e8c7ba7e2df2823eddbb770
lolsonic1234 <anthonydaluz@hotmail.com>
2015-12-06 16:59:52 -0500

    Update profile.js

--- profile.js
+++ profile.js
@@ -1,1 +1,29 @@
-console.log("testing");
+console.log("Javascript loaded up!");
+
+$(document).ready(function(){
+    console.log("Jquery Opened Up!");
+});
+
+$('.rules').click(function(){
+    
+    $( '.bioOpener' ).css( "display", "none" );
+    $( '.rulesOpener' ).css( "display", "block" );
+    $( '.fafOpener' ).css( "display", "none" );
+    
+});
+
+$('.bio').click(function(){
+    
+    $( '.rulesOpener' ).css( "display", "none" );
+    $( '.bioOpener' ).css( "display", "block" );
+    $( '.fafOpener' ).css( "display", "none" );
+    
+});
+
+$('.faf').click(function(){
+    
+    $( '.rulesOpener' ).css( "display", "none" );
+    $( '.fafOpener' ).css( "display", "block" );
+    $( '.bioOpener' ).css( "display", "none" );
+    
+});
