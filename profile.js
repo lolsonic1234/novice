f3d354af3507819c36ad0ac9fe007c5cb2ac77d5
lolsonic1234 <anthonydaluz@hotmail.com>
2015-12-09 22:51:07 -0500

    Update profile.js

--- profile.js
+++ profile.js
@@ -6,24 +6,60 @@ $(document).ready(function(){
 
 $('.rules').click(function(){
     
+    $( '#novice' ).css( "display", "none" );
+    $( '#novice' ).css( "height", "0px" );
     $( '.bioOpener' ).css( "display", "none" );
+    $( '.bioOpener' ).css( "height", "0px" );
+    $( '.fafOpener' ).css( "height", "0px" );
     $( '.rulesOpener' ).css( "display", "block" );
+    $( '.rulesOpener' ).animate({height: "600px"});
     $( '.fafOpener' ).css( "display", "none" );
+    console.log(".rules was Clicked");
     
 });
 
 $('.bio').click(function(){
     
     $( '.rulesOpener' ).css( "display", "none" );
+    $( '.rulesOpener' ).css( "height", "0px" );
     $( '.bioOpener' ).css( "display", "block" );
+    $( '.bioOpener' ).animate({height: "600px"});
     $( '.fafOpener' ).css( "display", "none" );
+    $( '.fafOpener' ).css( "height", "0px" );
+    console.log(".bio was Clicked");
     
 });
 
 $('.faf').click(function(){
     
+    $( '#novice' ).css( "display", "none" );
+    $( '#novice' ).css( "height", "0px" );
     $( '.rulesOpener' ).css( "display", "none" );
+    $( '.rulesOpener' ).css( "height", "0px" );
     $( '.fafOpener' ).css( "display", "block" );
+    $( '.fafOpener' ).animate({height: "600px"});
     $( '.bioOpener' ).css( "display", "none" );
+    $( '.bioOpener' ).css( "height", "0px" );
+    console.log(".faf was Clicked");
+    
+});
+
+$('.bioButtonTextN').click(function(){
+    
+    $( '#soullinker' ).css( "display", "none" );
+    $( '#soullinker' ).css( "height", "0px" );
+    $( '#novice' ).css( "display", "block" );
+    $( '#novice' ).animate({height: "600px"});
+    console.log(".bioButtonN was Clicked");
+    
+});
+
+$('.bioButtonTextS').click(function(){
+    
+    $( '#novice' ).css( "display", "none" );
+    $( '#novice' ).css( "height", "0px" );
+    $( '#soullinker' ).css( "display", "block" );
+    $( '#soullinker' ).animate({height: "600px"});
+    console.log(".bioButtonS was Clicked");
     
 });
